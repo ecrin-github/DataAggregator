52bb617027c86647f91774bb6bf718c85592a168
Steve Canham <stevecanham@outlook.com>
2020-05-17 16:49:54 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DataAggregator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
